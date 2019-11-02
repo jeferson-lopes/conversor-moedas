ebd56ee459a3b4df2b28d2c41357bb2a9b5fc2d2
JefersonLopes1 <JefersonLopes1>
2019-11-02 16:08:15 -0300

    Subindo arquivos

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Conversor from "./componentes/Conversor.js"
+
+function App() {
+  return (
+
+    <div className="App">
+      <h1>Conversor de Moedas</h1>
+      <div className="linha">
+        <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+      </div>
+      <div className="linha">
+        <Conversor moedaA="CAD" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="CAD"></Conversor>
+      </div>
+      <div className="linha">
+        <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="EUR"></Conversor>
+      </div>
+    </div>
+  );
+}
+
+export default App;
